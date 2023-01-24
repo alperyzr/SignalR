11784c85de42ddb86a663ed07a0639c50b5172d6
alper yazır <alper.yazir@outlook.com>
2023-01-24 23:45:18 +0300

    update

--- SignalR.Web/wwwroot/js/Home/Index.js
+++ SignalR.Web/wwwroot/js/Home/Index.js
@@ -2,7 +2,11 @@
 $(document).ready(() => {
     debugger;
     //Hub bağlantıı için kullanılır (API url üzerindeki hub methoduna gönderilir)
-    var connection = new signalR.HubConnectionBuilder().configureLogging(signalR.LogLevel.Debug).withUrl("https://localhost:7088/MyHub").build();
+    //configureLogging(signalR.LogLevel.Debug) console ekranına logları yazmak için kullanılır
+    //withUrl belirtilen URl e istek atması için kullanılır
+    //build bağlantıyı inşa etmek için kullanılır
+    //withAutomaticReconnect() bağlantı kesilirse belirli periotlarlarla tekrar bağlanmaya çalışmasını sağlar
+    var connection = new signalR.HubConnectionBuilder().configureLogging(signalR.LogLevel.Debug).withAutomaticReconnect().withUrl("https://localhost:7088/MyHub").build();
    
     //BAĞLANTI DURUMUNU EKRANA YAZDIRMA
     $("#conStatus").text(connection.q);
@@ -11,11 +15,43 @@ $(document).ready(() => {
     //Bağlantı start verilir. Then methodu success, catch methodu ise fail durumunda karşılayacağımız method olacak
     connection.start().then((e) => {       
         $("#conStatus").text(connection.q);
+        $("#loadingIcon").hide();
     }).catch((err) => {       
         console.log(err);
     });
 
     $("#conStatus").text(connection.q);
+
+    //Girişten itibaren bağlantı yoksa belirli aralıklarla bağlanmaya çalışması için
+    function Start() {
+        connection.start().then((e) => {
+            $("#conStatus").text(connection.q);
+            $("#loadingIcon").hide();
+        }).catch((err) => {
+            console.log(err);
+            //Başlangıçta bağlantıda bir sorun varsa 2sn sonra tekrar dener
+            setTimeout(() => Start(), 2000)
+        });
+    }
+
+    //Bu kısımlar bağlantı arada koparsa devreye girecektir ve tekrar bağlanmaya çalışığ bağlanamazsa kapatacaktır
+    //Bağlanmya çalışma, bağlanma ve çbağlantı kapatılma durumlarında loader gösterip console hataları yazdırır
+    connection.onreconnecting(err => {
+        $("#loadingIcon").show();
+        $("#conStatus").text(connection.q);
+        console.log("onrecoonnection: " + err);
+    });
+    connection.onreconnected(connectionId => {
+        $("#loadingIcon").hide();
+        $("#conStatus").text(connection.q);
+        console.log("ConnectionId: " + connectionId);
+    });
+    connection.onclose(() => {
+        $("#loadingIcon").hide();
+        $("#conStatus").text(connection.q); 
+        // 4 denemesinde Bağlantı tamamen kapandıktan sonra tekrar başlatılması sağlanabilir
+        Start();
+    });
 
     $("#btnNameSave").click(function () {
 
